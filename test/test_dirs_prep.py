804962769244dec28aeef41e33db5b1cbb4fee44
Paul Curry <pcurry@gmail.com>
2011-03-20 02:01:04 -0400

    Script with function stubs to prep test directories.

---
+++ test/test_dirs_prep.py
@@ -0,0 +1,18 @@
+import os
+import os.path
+
+def generateTestStructureNoLinks(path):
+    """ Generate the directory structure to be used to test the detector.
+    """
+    pass
+    
+def generateTestStructureWithSymLinks(path):
+    """ Generate the directory structure to be used to test the detector.
+    """
+    pass
+    
+def generateTestFiles(path):
+    """ Generate some sample files to test the detector.
+    """
+    pass
+
