b29b233ee41f35d993eca701087f45d234a657cb
Paul Curry <pcurry@gmail.com>
2011-04-03 03:11:44 -0400

    Create a directory structure to use for testing.

--- test/test_dirs_prep.py
+++ test/test_dirs_prep.py
@@ -1,8 +1,31 @@
 import os
 import os.path
 
-def generateTestStructureNoLinks(path):
+def generateTestStructureNoLinks(basepath):
     """ Generate the directory structure to be used to test the detector.
+
+    @return: A list of the directories created.
+    """
+    # Generate the paths, then create the directories.
+    dirname_format = "test-layer%d-dir%d"
+    per_layer = layer_depth = 4  # Testing directory structure size parameters
+    layers = [[basepath]] # Start with the base path
+    for i in range(1, layer_depth + 1):
+        this_layer = []
+        for path in layers[i-1]:
+            this_layer += [ os.path.join(path, dirname_format % (i,j))
+                            for j in range(per_layer) ]
+        layers.append(this_layer)
+    layers = layers[1:]
+    return_value = []
+    for layer in layers:
+        for path in layer:
+            os.mkdir(path)
+        return_value += layer
+    return return_value
+    
+def cleanTestStructureNoLinks(path_list):
+    """ Clean out the generated path structure.
     """
     pass
     
