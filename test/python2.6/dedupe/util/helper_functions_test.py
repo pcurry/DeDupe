b196b4c598433afbf4ac24062ea0ae8e7c112615
Paul Curry <pcurry@gmail.com>
2011-05-16 15:32:33 -0400

    Two tests done for add_or_append.

--- test/python2.6/dedupe/util/helper_functions_test.py
+++ test/python2.6/dedupe/util/helper_functions_test.py
@@ -1,5 +1,7 @@
+
 import unittest
 
+# Module under test
 import dedupe.util.helper_functions as helper_functions
 
 class TestAddOrAppend(unittest.TestCase):
@@ -13,7 +15,21 @@ class TestAddOrAppend(unittest.TestCase):
         self.assert_(test_key not in test_dict)
         helper_functions.add_or_append(test_key, test_value, test_dict)
         self.assert_(test_key in test_dict)
-        self.assertEqual(test_value, test_dict[test_key])
+        self.assertEqual([test_value], test_dict[test_key])
     
+        
     def testAppend(self):
-        pass
+        test_key = 1
+        test_value_1 = 'one'
+        test_value_2 = 'two'
+        test_dict = {test_key : [test_value_1]}
+        self.assertEquals(1, len(test_dict[test_key]))
+        helper_functions.add_or_append(test_key, test_value_2, test_dict)
+        self.assertEquals(2, len(test_dict[test_key]))
+        self.assertEquals([test_value_1, test_value_2], test_dict[test_key])
+    
+
+if __name__ == "__main__":
+    unittest.main()
+    
+            
