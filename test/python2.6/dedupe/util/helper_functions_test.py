a1c60c707def7049b72bb9432eef051b4f510471
Paul Curry <pcurry@gmail.com>
2011-05-15 20:49:46 -0400

    One test written.

--- test/python2.6/dedupe/util/helper_functions_test.py
+++ test/python2.6/dedupe/util/helper_functions_test.py
@@ -1,1 +1,19 @@
 import unittest
+
+import dedupe.util.helper_functions as helper_functions
+
+class TestAddOrAppend(unittest.TestCase):
+    """ Test that add_or_append works sanely.
+    """
+
+    def testAdd(self):
+        test_key = 1
+        test_value = "one"
+        test_dict = {}
+        self.assert_(test_key not in test_dict)
+        helper_functions.add_or_append(test_key, test_value, test_dict)
+        self.assert_(test_key in test_dict)
+        self.assertEqual(test_value, test_dict[test_key])
+    
+    def testAppend(self):
+        pass
